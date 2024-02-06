d46851babd33920838211231f2a0bd31b2e75fb6
Shinya Yamasaki <152153760+yksy97@users.noreply.github.com>
2024-02-06 07:09:03 +0000

    ログイン画面の修正

---
+++ app/javascript/packs/validations.js
@@ -0,0 +1,19 @@
+// このファイルはapplication.html.erbで使用しています
+
+(function() {
+  'use strict';
+
+  window.addEventListener('load', function() {
+    var forms = document.getElementsByClassName('needs-validation');
+
+    Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+})();
