14131f6d8afd600ae0e9965d22118837585e9ee3
Shinya Yamasaki <152153760+yksy97@users.noreply.github.com>
2024-02-06 11:19:12 +0000

    ajax

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -1,14 +1,28 @@
 class Public::RegistrationsController < Devise::RegistrationsController
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  # 会員登録後の遷移先
+  def after_sign_up_path_for(resource)
+    customer_path(resource)
+  end
+
+  def create
+    super do |resource|
+      if request.xhr?
+        if resource.persisted?
+          # 登録成功(Ajax)
+          render json: { status: 'success', location: after_sign_up_path_for(resource) } and return
+        else
+          # 登録失敗(Ajax)
+          render json: { status: 'error', errors: resource.errors.full_messages }, status: :unprocessable_entity and return
+        end
+      end
+    end
+  end
+
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password])
-  end
-
-   # 会員登録後の遷移先
-  def after_sign_up_path_for(resource)
-    customer_path(resource)
   end
 end
--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,9 +2,26 @@ class Public::SessionsController < Devise::SessionsController
   before_action :configure_permitted_parameters
   before_action :reject_customer, only: [:create]
 
-    # ログイン後の遷移先
+  # ログイン後の遷移先
   def after_sign_in_path_for(resource)
     customer_path(resource)
+  end
+
+  def create
+    self.resource = warden.authenticate!(auth_options)
+    if resource
+      set_flash_message!(:notice, :signed_in)
+      sign_in(resource_name, resource)
+      respond_to do |format|
+        format.html { redirect_to after_sign_in_path_for(resource) }
+        format.json { render json: { success: true, redirect_path: after_sign_in_path_for(resource) } }
+      end
+    else
+      respond_to do |format|
+        format.html { super }
+        format.json { render json: { success: false, errors: ["ログインに失敗しました。メールアドレスまたはパスワードが正しくありません。"] }, status: :unauthorized }
+      end
+    end
   end
 
   protected
@@ -16,15 +33,18 @@ class Public::SessionsController < Devise::SessionsController
   def reject_customer
     @customer = Customer.find_by(email: params[:customer][:email])
 
-    if @customer
-      if @customer.valid_password?(params[:customer][:password]) && !@customer.is_deleted
-        flash[:notice] = "退会済みです。"
-        redirect_to new_customer_registration_path
-      else
-        flash[:notice] = "正しい登録情報を入力してください。"
+    if @customer && !@customer.valid_password?(params[:customer][:password])
+      respond_to do |format|
+        format.html { super }
+        format.json { render json: { success: false, errors: ["退会済みです。または正しい登録情報を入力してください。"] }, status: :unauthorized }
       end
-    else
-      flash[:notice] = "会員情報が見つかりません。再度会員登録をお願いします。"
+      return
+    elsif !@customer
+      respond_to do |format|
+        format.html { super }
+        format.json { render json: { success: false, errors: ["会員情報が見つかりません。再度会員登録をお願いします。"] }, status: :not_found }
+      end
+      return
     end
   end
 end
