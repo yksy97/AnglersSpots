67bd1d26823e662a3d58bfbe74022a7945a9da01
Shinya Yamasaki <152153760+yksy97@users.noreply.github.com>
2024-02-06 11:49:37 +0000

    ajax

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,26 +2,9 @@ class Public::SessionsController < Devise::SessionsController
   before_action :configure_permitted_parameters
   before_action :reject_customer, only: [:create]
 
-  # ログイン後の遷移先
+    # ログイン後の遷移先
   def after_sign_in_path_for(resource)
     customer_path(resource)
-  end
-
-  def create
-    self.resource = warden.authenticate!(auth_options)
-    if resource
-      set_flash_message!(:notice, :signed_in)
-      sign_in(resource_name, resource)
-      respond_to do |format|
-        format.html { redirect_to after_sign_in_path_for(resource) }
-        format.json { render json: { success: true, redirect_path: after_sign_in_path_for(resource) } }
-      end
-    else
-      respond_to do |format|
-        format.html { super }
-        format.json { render json: { success: false, errors: ["ログインに失敗しました。メールアドレスまたはパスワードが正しくありません。"] }, status: :unauthorized }
-      end
-    end
   end
 
   protected
@@ -33,18 +16,15 @@ class Public::SessionsController < Devise::SessionsController
   def reject_customer
     @customer = Customer.find_by(email: params[:customer][:email])
 
-    if @customer && !@customer.valid_password?(params[:customer][:password])
-      respond_to do |format|
-        format.html { super }
-        format.json { render json: { success: false, errors: ["退会済みです。または正しい登録情報を入力してください。"] }, status: :unauthorized }
+    if @customer
+      if @customer.valid_password?(params[:customer][:password]) && !@customer.is_deleted
+        flash[:notice] = "退会済みです。"
+        redirect_to new_customer_registration_path
+      else
+      #   flash[:notice] = "正しい登録情報を入力してください"
       end
-      return
-    elsif !@customer
-      respond_to do |format|
-        format.html { super }
-        format.json { render json: { success: false, errors: ["会員情報が見つかりません。再度会員登録をお願いします。"] }, status: :not_found }
-      end
-      return
+    else
+      # flash[:notice] = "登録情報が見つかりません"
     end
   end
 end
