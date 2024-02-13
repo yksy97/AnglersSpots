4669ea20f26890c54bc6d1fda71b4a80029c9481
Shinya Yamasaki <152153760+yksy97@users.noreply.github.com>
2024-02-13 15:25:19 +0000

    リレーションシップス非同期まで後少し

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -2,12 +2,15 @@ class Public::RelationshipsController < ApplicationController
   before_action :authenticate_customer!
 
   def create
-    @customer = Customer.find(params[:customer_id])
+  @customer = Customer.find(params[:customer_id])
+  unless current_customer.following?(@customer)
     current_customer.follow(@customer)
     respond_to do |format|
       format.js
     end
   end
+end
+
   
  def destroy
     @customer = Customer.find(params[:customer_id])
