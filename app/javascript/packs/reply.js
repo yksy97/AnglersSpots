0b3f952b76a02f875faeaaf97a0ddb15b0dcbc4b
Shinya Yamasaki <152153760+yksy97@users.noreply.github.com>
2024-02-16 05:41:16 +0000

    投稿詳細画面のレイアウト

--- app/javascript/packs/reply.js
+++ app/javascript/packs/reply.js
@@ -1,10 +1,19 @@
 // posts/showのリプライ機能
 document.addEventListener("DOMContentLoaded", function() {
+  // 返信
   document.querySelectorAll(".reply-link").forEach(function(link) {
     link.addEventListener("click", function(e) {
       e.preventDefault();
       var commentId = this.dataset.commentId;
       document.getElementById("reply-form-" + commentId).style.display = "block";
+    });
+  });
+
+  // 閉じる
+  document.querySelectorAll(".close-reply-form").forEach(function(button) {
+    button.addEventListener("click", function() {
+      var commentId = this.dataset.commentId;
+      document.getElementById("reply-form-" + commentId).style.display = "none";
     });
   });
 });
