239600122b451a656a2fb150dfe74ceef08b8b44
Shinya Yamasaki <152153760+yksy97@users.noreply.github.com>
2024-02-19 08:07:43 +0000

    魚種一覧・通知一覧の作成

--- app/controllers/public/notifications_controller.rb
+++ app/controllers/public/notifications_controller.rb
@@ -1,5 +1,9 @@
 class Public::NotificationsController < ApplicationController
   before_action :authenticate_customer!
+  
+  def index
+    @notifications = current_customer.notifications.order(created_at: :desc)
+  end
 
   def update
     notification = current_customer.notifications.find(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'genres/index'
+  get 'genres/create'
   # 顧客用Deviseルーティング
   devise_for :customers, skip: [:passwords], controllers: {
     registrations: 'public/registrations',
@@ -10,34 +12,35 @@ Rails.application.routes.draw do
     sessions: 'admin/sessions'
   }
 
-  scope module: :public do
-    root to: 'homes#top'
-    get 'about', to: 'homes#about'
+scope module: :public do
+  root to: 'homes#top'
+  get 'about', to: 'homes#about'
 
-    # confirmとwithdrawはcustomerブロックの外側に。退会処理に特定のIDは必要ないため。
-    get "customers/confirm" => "customers#confirm"
-    patch "customers/withdraw" => "customers#withdraw"
-    
-    resources :customers, only: [:show, :edit, :update] do
-      resource :relationships, only: [:create, :destroy]
-      get 'followings' => 'relationships#followings', as: 'followings'
-      get 'followers' => 'relationships#followers', as: 'followers'
-    end
-
-    resources :posts, only: [:index, :show, :edit, :create, :destroy, :update] do
-      resources :post_comments, only: [:create, :edit, :destroy, :update]
-      resource :favorites, only: [:create, :destroy]
-      collection do
-        # 「ransack」 matchメゾット、viaオプションでGETとPOSTを指定
-        match 'search' => 'posts#search', via: [:get, :post], as: :search
-      end
-    end
-
-    resources :genres, only: [] do
-      get 'posts', to: 'posts#genre', as: 'posts', on: :member
-    end
-    
-    resources :tackles, only: [:new, :create, :index, :edit, :update, :destroy]
-    
+  # confirmとwithdrawはcustomerブロックの外側に。退会処理に特定のIDは必要ないため。
+  get "customers/confirm" => "customers#confirm"
+  patch "customers/withdraw" => "customers#withdraw"
+  
+  resources :customers, only: [:show, :edit, :update] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
   end
+
+  resources :posts, only: [:index, :show, :edit, :create, :destroy, :update] do
+    resources :post_comments, only: [:create, :edit, :destroy, :update]
+    resource :favorites, only: [:create, :destroy]
+    collection do
+      # 「ransack」 matchメゾット、viaオプションでGETとPOSTを指定
+      match 'search' => 'posts#search', via: [:get, :post], as: :search
+    end
+  end
+
+  resources :notifications, only: [:index]
+
+  resources :genres, only: [:index, :create, :edit, :update, :destroy] do
+    get 'posts', to: 'posts#genre', as: 'posts', on: :member
+  end
+  
+  resources :tackles, only: [:new, :create, :index, :edit, :update, :destroy]
+end
 end
