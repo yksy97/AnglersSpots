c213fd373cc1f96d3f5f4ac85ddf2f2f114c4ccc
Shinya Yamasaki <152153760+yksy97@users.noreply.github.com>
2024-02-24 17:40:11 +0000

    [add] 管理者画面のコメント管理

--- app/controllers/admin/post_comments_controller.rb
+++ app/controllers/admin/post_comments_controller.rb
@@ -7,6 +7,6 @@ class Admin::PostCommentsController < ApplicationController
 
   def destroy
     comment = PostComment.find(params[:id]).destroy
-    redirect_to admin_customer_post_path(comment.post.customer, comment.post_id), notice: 'コメントが削除されました'
+    redirect_to admin_post_comments_path(comment.post.customer, comment.post_id), notice: 'コメントが削除されました'
   end
 end
