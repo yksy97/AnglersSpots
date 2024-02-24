d60f910b290448e304de6c7a88285ab484b591cb
Shinya Yamasaki <152153760+yksy97@users.noreply.github.com>
2024-02-24 04:05:11 +0000

    [layout] 投稿詳細画面

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -2,101 +2,99 @@ class Public::PostsController < ApplicationController
   before_action :authenticate_customer!
   before_action :ensure_correct_customer, only: [:edit, :update, :destroy]
 
-  def new
-    @post = Post.new
-    @genres = Genre.order(:name)
-    @tackles = Tackle.all
-  end
-  
-  def index
-    if params[:rig_id]
-      @posts = Rig.find(params[:rig_id]).posts.includes(:customer, :genre).order(created_at: :desc).page(params[:page]).per(5)
-    elsif params[:location]
-      @posts = Post.where(location: params[:location]).includes(:customer, :genre).order(created_at: :desc).page(params[:page]).per(5)
-    else
-      @posts = Post.includes(:customer, :genre).order(created_at: :desc).page(params[:page]).per(5)
-    end
-    @post = Post.new
-    @genres = Genre.order(:name)
-    @tackles = Tackle.all
-  end
+def new
+  @post = Post.new
+  @genres = Genre.order(:name)
+  @tackles = Tackle.all
+end
 
-  def show
-    @post = Post.find(params[:id])
-    @customer = @post.customer
-    @post_comment = PostComment.new
-    @post_comments = @post.post_comments.order(created_at: :desc)
+def index
+  if params[:rig_id]
+    @posts = Rig.find(params[:rig_id]).posts.includes(:customer, :genre).order(created_at: :desc).page(params[:page]).per(5)
+  elsif params[:location]
+    @posts = Post.where(location: params[:location]).includes(:customer, :genre).order(created_at: :desc).page(params[:page]).per(5)
+  else
+    @posts = Post.includes(:customer, :genre).order(created_at: :desc).page(params[:page]).per(5)
   end
+  @post = Post.new
+  @genres = Genre.order(:name)
+  @tackles = Tackle.all
+end
 
-  def create
-  @post = current_customer.posts.build(post_params)
-
-  if params[:post][:new_genre_name].present?
-    genre = Genre.find_or_create_by(name: params[:post][:new_genre_name])
-    @post.genre_id = genre.id
-  end
-
-  respond_to do |format|
-    if @post.save
-      @post.save_rigs(params[:post][:rig_list])
-      format.html { redirect_to posts_path, notice: "投稿が完了しました" }
-      format.js
-    else
-      @posts = Post.includes(:customer, :genre).order(created_at: :desc)
-      @my_posts = current_customer.posts.order(created_at: :desc)
-      @genres = Genre.order(:name)
-      @tackles = Tackle.all
-
-      format.html { render :index }
-      format.js
-    end
-  end
-  end
-
-
-  def genre
-    genre = Genre.find(params[:id])
-    @posts = genre.posts
-  end
-
-  def edit
-    @post = Post.find(params[:id])
-  end
-
-  def update
-    @post = Post.find(params[:id])
-    if @post.update(post_params)
-      redirect_to post_path(@post), notice: "投稿が更新されました"
-    else
-      render :edit
-    end
-  end
-  
-  def tackle_selection
+def show
   @post = Post.find(params[:id])
+  @customer = @post.customer
+  @post_comment = PostComment.new
+  @post_comments = @post.post_comments.order(created_at: :desc)
+end
+
+def create
+  @post = current_customer.posts.build(post_params)
+if params[:post][:new_genre_name].present?
+  genre = Genre.find_or_create_by(name: params[:post][:new_genre_name])
+  @post.genre_id = genre.id
+end
+# 非同期通信
+respond_to do |format|
+  if @post.save
+    @post.save_rigs(params[:post][:rig_list])
+    format.html { redirect_to posts_path, notice: "投稿が完了しました" }
+    format.js
+  else
+  @posts = Post.includes(:customer, :genre).order(created_at: :desc)
+  @my_posts = current_customer.posts.order(created_at: :desc)
+  @genres = Genre.order(:name)
+  @tackles = Tackle.all
+  format.html { render :index }
+  format.js
+  end
+end
+end
+
+def genre
+  genre = Genre.find(params[:id])
+  @posts = genre.posts
+end
+
+def edit
+  @post = Post.find(params[:id])
+end
+
+def update
+@post = Post.find(params[:id])
+  if @post.update(post_params)
+    redirect_to post_path(@post), notice: "投稿が更新されました"
+  else
+    render :edit
+  end
+end
+
+def tackle_selection
+@post = Post.find(params[:id])
   if @post.update(tackle_id: params[:post][:tackle_id])
     redirect_to @post, notice: '登録したタックルが適用されました'
   else
     render :edit, alert: '登録したタックルの適用に失敗しました'
   end
-  end
+end
 
-  def destroy
-    @post = Post.find(params[:id])
-    @post.destroy
-    redirect_to posts_path, notice: "投稿が削除されました"
-  end
+def destroy
+  @post = Post.find(params[:id])
+  @post.destroy
+  redirect_to posts_path, notice: "投稿が削除されました"
+end
 
-  private
+private
 
-  def post_params
-    params.require(:post).permit(:body, :image, :genre_id, :location, :tackle_id, :rig_list)
-  end
+def post_params
+  params.require(:post).permit(:body, :image, :genre_id, :location, :tackle_id, :rig_list)
+end
 
-  def ensure_correct_customer
-    @post = Post.find(params[:id])
-    unless @post.customer == current_customer
-      redirect_to posts_path, alert: '権限がありません。'
-    end
-  end
+def ensure_correct_customer
+@post = Post.find(params[:id])
+unless @post.customer == current_customer
+  redirect_to posts_path, alert: '権限がありません。'
+end
+
+end
 end
--- app/controllers/public/tackles_controller.rb
+++ app/controllers/public/tackles_controller.rb
@@ -1,21 +1,21 @@
 class Public::TacklesController < ApplicationController
   before_action :authenticate_customer!
   before_action :set_tackle, only: [:edit, :update, :destroy]
-  
-  def new
-    @tackle = Tackle.new
-  end
-  
-  def index
-    @tackles = current_customer.tackles.order(created_at: :desc).page(params[:page]).per(5)
-  end
-  
+
+def new
+  @tackle = Tackle.new
+end
+
+def index
+  @tackles = current_customer.tackles.order(created_at: :desc).page(params[:page]).per(5)
+end
+
 def create
   @tackle = current_customer.tackles.build(tackle_params)
   respond_to do |format|
     if @tackle.save
-      format.js
-      format.html { redirect_to tackles_path, notice: 'タックルが登録されました' }
+     format.js
+     format.html { redirect_to tackles_path, notice: 'タックルが登録されました' }
     else
       format.js 
       format.html { render :new }
@@ -23,32 +23,33 @@ def create
   end
 end
 
-  def edit
-    @tackle = Tackle.find(params[:id])
-  end
+def edit
+  @tackle = Tackle.find(params[:id])
+end
 
-  def update
-    @tackle = Tackle.find(params[:id])
+def update
+  @tackle = Tackle.find(params[:id])
     if @tackle.update(tackle_params)
-      redirect_to tackles_path, notice: 'タックルが更新されました'
+    redirect_to tackles_path, notice: 'タックルが更新されました'
     else
-      render :edit
+    render :edit
     end
-  end
+end
 
-  def destroy
-    @tackle = Tackle.find(params[:id])
-    @tackle.destroy
-    redirect_to root_path, notice: 'タックルが削除されました'
-  end
+def destroy
+  @tackle = Tackle.find(params[:id])
+  @tackle.destroy
+  redirect_to root_path, notice: 'タックルが削除されました'
+end
 
-  private
+private
 
-  def tackle_params
-    params.require(:tackle).permit(:name, :rod, :reel, :line, :bait)
-  end
-  
-  def set_tackle
-    @tackle = current_customer.tackles.find(params[:id])
-  end
+def tackle_params
+  params.require(:tackle).permit(:name, :rod, :reel, :line, :bait)
+end
+
+def set_tackle
+  @tackle = current_customer.tackles.find(params[:id])
+end
+
 end
