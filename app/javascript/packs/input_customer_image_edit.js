5cd2a2610fd0808a11da96912f946ccd57fab9ed
Shinya Yamasaki <152153760+yksy97@users.noreply.github.com>
2024-02-25 11:48:27 +0000

    [layout] customers/show

---
+++ app/javascript/packs/input_customer_image_edit.js
@@ -0,0 +1,31 @@
+var CustomerDropArea = document.querySelector('.customer-edit-drop-area');
+var CustomerInputFile = document.getElementById('PostsEditInputFile');
+
+['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
+  CustomerDropArea.addEventListener(eventName, preventDefaults, false);
+});
+
+function preventDefaults(e) {
+  e.preventDefault();
+  e.stopPropagation();
+}
+
+// ドロップ時の処理
+CustomerDropArea.addEventListener('drop', handleDrop, false);
+
+function handleDrop(e) {
+  var dt = e.dataTransfer;
+  var files = dt.files;
+
+  CustomerInputFile.files = files;
+
+  // ファイル名を表示
+  var fileName = files[0].name;
+  CustomerDropArea.querySelector('.custom-file-label').innerText = fileName;
+}
+
+CustomerInputFile.addEventListener('change', function(e){
+  var files = CustomerInputFile.files;
+  var fileName = files[0].name;
+  CustomerDropArea.querySelector('.custom-file-label').innerText = fileName;
+});
