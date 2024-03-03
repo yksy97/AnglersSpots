c0e6012503bb03cec53eaac81e53e623febde381
Shinya Yamasaki <152153760+yksy97@users.noreply.github.com>
2024-03-03 13:51:40 +0000

    [add] tide GoogleMap

--- app/controllers/public/tides_controller.rb
+++ app/controllers/public/tides_controller.rb
@@ -4,13 +4,16 @@ require 'net/https'
 
 class Public::TidesController < ApplicationController
   def index
-    @areas = Tide.all.pluck(:prefecture_name, :prefecture_code).uniq!
+    @areas = Tide.all.pluck(:prefecture_name, :prefecture_code).uniq! # エリアの一覧
   end
   
   def graf
-    @areas = Tide.all.pluck(:prefecture_name, :prefecture_code).uniq!
+    # パラメーターがない場合、indexにリダイレクト
+    redirect_to tides_path and return if params[:prefecture_code].blank? || params[:port_code].blank? || params[:date].blank?
     
-    # APIのURL作成
+    @areas = Tide.all.pluck(:prefecture_name, :prefecture_code).uniq! # エリアの一覧
+    
+    # APIのURL
     api_url = "https://tide736.net/api/get_tide.php"
 
     # APIリクエスト用のパラメータ
@@ -29,11 +32,17 @@ class Public::TidesController < ApplicationController
     https.use_ssl = true
     request = Net::HTTP::Get.new(uri.request_uri)
     response = https.request(request)
-    @response_body = JSON.parse(response.body)
+    response_body = JSON.parse(response.body)
+
     # APIレスポンス出力
-    if @response_body['status'] != 1
-      raise @response_body['message']
+    if response_body['status'] != 1
+      raise response_body['message']
     end
+    
+    @tides = response_body['tide']['chart'][params[:date]]['tide'].map{|data| [data['time'], data['cm']]} # 潮位
+    @tide = response_body['tide']['chart'][params[:date]]['moon']['title'] # 潮汐
+    @astro_twilight = response_body['tide']['chart'][params[:date]]['sun']['astro_twilight'] # 日の出・日の入
+    @location = [response_body['tide']['port']['latitude'], response_body['tide']['port']['longitude']]
     
     render 'index'
   end
