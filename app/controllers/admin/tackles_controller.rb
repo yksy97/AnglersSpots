ed28d1a70f6e91eaaa4cf60fa032c74677e47402
Shinya Yamasaki <152153760+yksy97@users.noreply.github.com>
2024-03-05 22:32:14 +0000

    [add] 管理者タックル画面

---
+++ app/controllers/admin/tackles_controller.rb
@@ -0,0 +1,26 @@
+class Admin::TacklesController < ApplicationController
+  before_action :authenticate_admin!
+
+  def index
+    @tackles = Tackle.order(created_at: :desc).page(params[:page]).per(6)
+  end
+  
+  def show
+    @tackle = Tackle.find(params[:id])
+  end
+
+  def destroy
+    @tackle = Tackle.find(params[:id])
+    if @tackle.destroy
+      redirect_to admin_tackles_path, notice: 'タックルが削除されました'
+    else
+      redirect_to admin_tackles_path, alert: 'タックルの削除に失敗しました'
+    end
+  end
+
+  private
+
+  def tackle_params
+    params.require(:tackle).permit(:name)
+  end
+end
---
+++ app/helpers/admin/tackles_helper.rb
@@ -0,0 +1,2 @@
+module Admin::TacklesHelper
+end
