082c8084a8f03a9599673feb0b0c787c717755c9
Shinya Yamasaki <152153760+yksy97@users.noreply.github.com>
2024-03-06 06:52:32 +0000

    [fix] ransack

--- app/models/tackle.rb
+++ app/models/tackle.rb
@@ -8,9 +8,9 @@ class Tackle < ApplicationRecord
   
   validates :name, presence: true, uniqueness: { scope: :customer_id, message: "このタックル名はすでに存在します" }
 
-  # def self.ransackable_attributes(auth_object = nil)
-  #   ["name", "rod", "reel", "line", "bait"]
-  # end
+  def self.ransackable_attributes(auth_object = nil)
+    ["name"]
+  end
   
   def self.ransackable_associations(auth_object = nil)
     ["posts"]
