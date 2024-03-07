469297db65bd4a95518620c960e155c9eec23da9
Shinya Yamasaki <152153760+yksy97@users.noreply.github.com>
2024-03-07 11:18:07 +0000

    [layout] 微調整

--- db/migrate/20240217150758_create_tackles.rb
+++ db/migrate/20240217150758_create_tackles.rb
@@ -2,9 +2,7 @@ class CreateTackles < ActiveRecord::Migration[6.1]
   def change
     create_table :tackles do |t|
       t.string :name
-
       t.references :customer, null: false, foreign_key: true
-
       t.timestamps
     end
   end
