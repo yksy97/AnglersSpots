3cae1a0c50e5e4669a8a875e1b3095a2ec101332
Shinya Yamasaki <152153760+yksy97@users.noreply.github.com>
2024-03-14 12:11:23 +0000

    インデント整理

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,8 +1,3 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-
 import Rails from "@rails/ujs"
 // import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
@@ -17,6 +12,6 @@ import "../stylesheets/application";
 require("@nathanvda/cocoon")
 
 Rails.start()
-// JSとの競合を考慮した結果、TurbolinksをONにするメリットより、OFFにするメリットが上回っているのでOFFにしました。
+// JSとの競合を考慮してOFFにしました。
 // Turbolinks.start()
 ActiveStorage.start()
--- app/models/post_comment.rb
+++ app/models/post_comment.rb
@@ -3,11 +3,11 @@ class PostComment < ApplicationRecord
   belongs_to :post
   validates :comment, presence: true
   has_many :notifications, as: :notifiable, dependent: :destroy
-  
+
   # リプライ機能
   belongs_to :parent, class_name: "PostComment", optional: true
   has_many :replies, class_name: "PostComment", foreign_key: "parent_id", dependent: :destroy
-  
+
   # belongs_toの場合
   def self.ransackable_attributes(auth_object = nil)
     ["comment"]
@@ -15,9 +15,9 @@ class PostComment < ApplicationRecord
   
   # コメント通知
   after_create :create_notification
-  
+
   private
-  
+
   def create_notification
     Notification.create(
     customer_id: self.post.customer_id,
