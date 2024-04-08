8dc071ca9f37c9aa8ab893708d7d83fe1113f180
Shinya Yamasaki <152153760+yksy97@users.noreply.github.com>
2024-04-08 12:37:21 +0000

    「Relearn」　　いいね機能

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -12,3 +12,19 @@ class Favorite < ApplicationRecord
     )
   end
 end
+
+
+
+# 「has_one :notification, as: :notifiable, dependent: :destroy」：いいねと通知は１：１だから「has_one」
+
+# 「validates_uniqueness_of :post_id, scope: :customer_id」：１つのPostに対して。同じCustomerが何度もいいねできないように制限する。一意性。
+# 「 validates_uniqueness_of」は、指定された属性の値が一意であることを確認する、Railsの機能。「scope」オプションと併せて使用する。
+# -Favoriteモデルのインスタンス（特定の投稿に対するいいね）という行為の一異性を、「Customer」という属性（Customer_id:どのCustomerによるいいねかを示す）に依存させるという意味。
+
+# Genreモデルの「validates :name, uniqueness: { scope: :customer_id, case_sensitive: false },」について、
+# ここでの「scope: :customer_id」は、各Customerが自分のジャンルを個別に管理するための記述。Genreモデルの重複を禁止する処理を、各Cutomerごとに行うので、AとBのCustomerが同じジャンル名をGenreモデルのデータベースに登録できる。
+
+# 「notifiable: self,」は、ポリモーフィック関連を使用して、どのいいね（Favoriteオブジェクト）が、この通知の原因であるかを指定している。
+# -「self」は、create_notificationメソッドを呼び出している現在のFavoriteオブジェクトを指定する。
+
+# 「read: false」は通知がまだ読まれていないこと（未読状態）を示す。これをCustomerが通知を確認後にこのフラグをtureに更新すると、既読状態に
