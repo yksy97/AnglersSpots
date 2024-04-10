da4771a097d5d778479395fb45427fddec06e81f
Shinya Yamasaki <152153760+yksy97@users.noreply.github.com>
2024-04-10 14:52:22 +0000

    「Relearn」　　フォロー/フォロワー機能　calss_nameオプションで明示的にモデルを指定する理由

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -56,3 +56,22 @@ class Customer < ApplicationRecord
   end
 
 end
+
+
+
+# ２５行目「has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy」の「class_name: "Relationship",」について
+# これは、関連するモデルがRelationshipであると、「明示的」にモデルのクラス名を指定している。
+# なぜ明示的に指定するのか。
+# -「Customer」モデルが「Relationship」モデルと2種類の異なる関係を持っているから。
+# -2種類の異なる関係とは、フォロー「Followings」とフォロワー「Followers」のこと。この２つは、内部的に「Relationship」モデルで表されている。
+# -Railsには、この「Followings」と「Followers」の関連から、「Relationship」モデルを直接推測することができないので、明示的に「Relationship」モデルを指定する。
+
+# 要するに、明示していない場合、「Relationship」モデルを介して、フォローしているCustomerの集まりを表現している「has_many :followings」を、Railsは「Followings」モデルだと判断して、
+# 存在しないモデルを探してしまう。そこで、「class_name」オプションを使って、Railsに正確なモデルを指定する必要がある。
+
+# ２９行目のフォローワー関係を記述する際も、reverse_of_relationshipsとfollowersという存在しないモデルを探さないように明示的に「Relationship」モデルを指定している。
+
+# ここでの、「has_many :relationships」と「has_many :reverse_of_relationships」は「Relationship」モデルを中間テーブルとして使用していて、
+# 「follower_id」を外部キーとして持っている。「follower_id」は、フォローしている、又は、フォローされているCustomer（自分自身）のIDのこと。
+
+# 「Relationship」モデルは中間テーブルとして、２つの「follower_id」の外部キーを持って、どのCustomerがどのCustomerをフォローしているかを記録し、多対多の関係が実現している。
